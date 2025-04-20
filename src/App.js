6092050a9f7bd680dad62da89b9e2616ae6057e0
danielegomesads <danielegomesdasneves@gmail.com>
2025-04-19 22:30:23 -0300

    Adicionado funcao de enter para o botao acessar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-//App.js
 import React, { useState } from 'react';
 import Button from './Button';
 import './App.css'
 
 function App() {
-  // Estados para armazenar email, senha e mensagem de feedback
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
   const [mensagem, setMensagem] = useState('');
 
-  // Função de validação ao clicar no botão
-  const validarLogin = () => {
+  // Função de validação ao clicar no botão ou pressionar Enter
+  const validarLogin = (event) => {
+    if (event) event.preventDefault(); // Evita o reload da página
     if (email === 'daniele.gomes@pucpr.br' && senha === '987654') {
       setMensagem('Acessado com sucesso!');
     } else {
@@ -21,26 +20,28 @@ function App() {
   return (
     <div style={{ maxWidth: '300px', margin: 'auto', marginTop: '100px' }}>
       <h2>Login</h2>
-      <input
-        type="email"
-        placeholder="E-mail"
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-        style={{ display: 'block', width: '100%', marginBottom: '10px' }}
-      />
-      <input
-        type="password"
-        placeholder="Senha"
-        value={senha}
-        onChange={e => setSenha(e.target.value)}
-        style={{ display: 'block', width: '100%', marginBottom: '10px' }}
-      />
-       <Button
-        onClick={validarLogin}
-        style={{ width: '100%' }}
-      >
-        Acessar
-      </Button>
+      <form onSubmit={validarLogin}>
+        <input
+          type="email"
+          placeholder="E-mail"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          style={{ display: 'block', width: '100%', marginBottom: '10px' }}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          value={senha}
+          onChange={e => setSenha(e.target.value)}
+          style={{ display: 'block', width: '100%', marginBottom: '10px' }}
+        />
+        <Button
+          type="submit"
+          style={{ width: '100%' }}
+        >
+          Acessar
+        </Button>
+      </form>
       <label style={{ display: 'block', marginTop: '20px' }}>{mensagem}</label>
     </div>
   );
